0accae98816dbdaae013897ba7db0a0c142698e0
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2018-09-12 19:40:12 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zerosCount = 0;
+
+  for (let i = 5; i <= number; i++) {
+    let iCopy = i;
+
+    while (iCopy % 5 === 0) {
+      iCopy /= 5;
+      zerosCount++;
+    }
+  }
+
+  return zerosCount;
 }
